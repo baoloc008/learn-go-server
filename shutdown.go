b17f98dc085fe1b43cc3ea39bd033d6b9c6409ef
baoloc008 <baoloc008@gmail.com>
2020-04-08 23:26:21 +0700

    refactor code

--- shutdown.go
+++ shutdown.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,23 +12,22 @@ import (
 
 func main() {
 	router := http.NewServeMux() // here we could also go with third party packages to create a router
-	router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello World!")
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
 	})
-	server := &http.Server{
+	srv := &http.Server{
 		Addr:    ":9620",
 		Handler: router,
 	}
 
-	// it’s not necessary to wrap another go-routine with server.ListenAndServe()
+	// it’s not necessary to wrap another go-routine with srv.ListenAndServe()
 	// this method is blocking and in the go documentation it already described that will use separate go-routine for each incoming request.
 	// The only reason I’m using go-routine to have another wrapper is because it’s more easier for me to handle the channel interactions and rest of the shutdown steps
 	go func() {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Server ListenAndServe Error: %v", err)
 		}
 	}()
-	log.Print("Server Started")
 
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
@@ -43,13 +41,13 @@ func main() {
 		cancel()
 	}()
 
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Server Shutdown Failed:%+v", err)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Error: %v", err)
 	}
 	log.Print("Server Shutdown Properly")
 }
 
-func basic() {
+func useChannel() {
 	done := make(chan os.Signal, 1)                                    // creates a done channel and it can only accept os.Signal type with 1 capacity
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM) // causes the package signal to relay incoming signals to done channel
 
